acbb57ceec01c8719e4a9cbc99343ca30d832003
shinjaewon99 <tlswodnjs99@naver.com>
2023-02-11 19:21:18 +0900

    feat : 초기설정

---
+++ shinChat/src/test/java/chat/shinChat/ShinChatApplicationTests.java
@@ -0,0 +1,13 @@
+package chat.shinChat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShinChatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
