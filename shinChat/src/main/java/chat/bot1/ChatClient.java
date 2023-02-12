276c2b5bbcb343c8fc9b8dd9db52c80f63eda4f7
shinjaewon99 <tlswodnjs99@naver.com>
2023-02-12 22:12:15 +0900

    refactor : 패키지 변경

--- shinChat/src/main/java/chat/bot1/ChatClient.java
+++ shinChat/src/main/java/chat/bot1/ChatClient.java
@@ -1,4 +1,4 @@
-package chat.bot;
+package chat.bot1;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
--- shinChat/src/main/java/chat/bot1/ChatServer.java
+++ shinChat/src/main/java/chat/bot1/ChatServer.java
@@ -1,4 +1,4 @@
-package chat.bot;
+package chat.bot1;
 
 import java.io.*;
 import java.net.ServerSocket;
